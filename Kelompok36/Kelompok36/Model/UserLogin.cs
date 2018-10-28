20f5aecd8bce526537365e1316da5935b19bce3b
aziz <uglyfatmonster@gmail.com>
2018-10-28 14:44:52 +0700

    aziz was here

---
+++ Kelompok36/Kelompok36/Model/UserLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace Kelompok36
+{
+    public static class UserLogin
+    {
+        public static string Username = "Kelompok36";
+        public static string Password = "pro958545";
+    }
+}
